54fb9b0bc78e050882621710a4ea05254cdb22a3
danielrebelo1 <daniel.rebelo2003@gmail.com>
2022-10-11 15:49:00 +0100

    changes

--- Code/test.cpp
+++ Code/test.cpp
@@ -1,2 +1,34 @@
+#include <iostream>
+using namespace std;
 
+void prog() {
+    cout << "Olá! Bem-vindo aos Horários de LEIC! O que desejas fazer ? (introduz o número) \n" << ;
+    cout << "1. Ver lista de ocupação de turmas em determinada UC\n ";
+    cout << "2. Horário de determinado estudante\n";
+    cout << "3. Estudantes de determinada turma\n";
+    cout << "4. Pedido de alteração de turma\n";
+    cout << "5. Ver lista número de estudantes com mais de n UCS\n";
 
+    int instruction1;
+
+    cin >> instruction1;
+
+    switch(instruction1)
+    {
+        case 1:
+
+            break;
+        case 2:
+
+            break;
+        case 3:
+
+            break;
+        case 4:
+
+            break;
+        case 5:
+
+            break;
+    }
+}
