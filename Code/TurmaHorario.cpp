e46658dbfbf31539e495d3728a78f7535ffad3f1
danielrebelo1 <daniel.rebelo2003@gmail.com>
2022-10-30 01:24:21 +0000

    algumas mudancas

--- Code/TurmaHorario.cpp
+++
@@ -1,9 +0,0 @@
-//
-// Created by jaimefrf on 18-10-2022.
-//
-
-#include "TurmaHorario.h"
-
-std::list<Slot> TurmaHorario::getTurma() const {
-    return HorarioTurma;
-}
--- Code/Utils.cpp
+++ Code/Utils.cpp
@@ -2,12 +2,35 @@
 // Created by Jaime on 29/10/2022.
 //
 
-#include "Utils.h"
+// #include "Utils.h"
+#include "FileReader.h"
+#include "Student.h"
 
+
+
+struct studentComparator2 {
+    bool operator()(Student* s1, Student* s2) const {
+        if (s1->get_student_Code() != s2->get_student_Code()) return (s2->get_Name() < s1->get_Name());
+        return false;
+    }
+};
+
+struct studentComparatorCode {
+    bool operator()(Student* s1, Student* s2) const {
+        if (s1->get_student_Code() != s2->get_student_Code()) return (s1->get_student_Code() < s2->get_student_Code());
+        return false;
+    }
+};
+
+struct studentComparatorDecreasingCode {
+    bool operator()(Student* s1, Student* s2) const {
+        if (s1->get_student_Code() != s2->get_student_Code()) return (s2->get_student_Code() < s1->get_student_Code());
+        return false;
+    }
+};
 
 
 map<std::string , int> weekDays{{"Monday", 1}, {"Tuesday", 2}, {"Wednesday", 3}, {"Thursday", 4}, {"Friday", 5}};
-
 
 bool sorterHorario(Slot *s1, Slot *s2){
     if(weekDays[s1->getDiaSemana()] != weekDays[s2->getDiaSemana()]) return (weekDays[s1->getDiaSemana()] < weekDays[s2->getDiaSemana()]);
@@ -36,5 +59,62 @@ std::string Fixer(std::string horarioInicio) {
     }
     else {
         return std::to_string((int)hInicio_f) + "h";
+    }
+}
+
+std::set<Student* , studentComparator> getStudentsYear(std::set<Student* , studentComparator> students , char year)
+{
+    std::set<Student* , studentComparator> students2;
+    for (Student* s : students){
+        std::vector<Turma*> v = s->get_TurmasAluno();
+        auto it = std::find_if(v.begin(),v.end(),[year](const Turma* t) {return (t->get_turmaCode()[0]) == year ;});
+        if (it != v.end()){students2.insert(s);}
+    }
+    return students2;
+}
+
+std::set<Student* , studentComparator2> StudentReverseSortAlpha(std::set<Student* , studentComparator> students){
+        std::set<Student* , studentComparator2> newstudents;
+        for (Student *s : students) {newstudents.insert(s);}
+        return newstudents;
+};
+
+std::set<Student* , studentComparatorCode> StudentSortCode(std::set<Student* , studentComparator> students){
+    std::set<Student* , studentComparatorCode> newstudents;
+    for (Student *s : students) {newstudents.insert(s);}
+    return newstudents;
+};
+
+std::set<Student* , studentComparatorDecreasingCode> StudentReverseSortCode(std::set<Student* , studentComparator> students){
+    std::set<Student* , studentComparatorDecreasingCode> newstudents;
+    for (Student *s : students) {newstudents.insert(s);}
+    return newstudents;
+};
+
+void PrintStudents(std::set<Student* , studentComparator> students , char option)
+{
+    std::set<Student* , studentComparator2> newstudents2;
+    std::set<Student* , studentComparatorCode> newstudents3;
+    std::set<Student* , studentComparatorDecreasingCode> newstudents4;
+    switch (option) {
+        case '1':
+            cout << "Nome" << setw(15) << "Número de estudante" << endl;
+            for (Student *s : students){cout << s->get_Name() << "\t" << setw(9) << s->get_student_Code() << endl;}
+            break;
+        case '2':
+            newstudents2 = StudentReverseSortAlpha(students);
+            cout << "Nome" << setw(15) << "Número de estudante" << endl;
+            for (Student *s : newstudents2){cout << s->get_Name() << "\t" << setw(9) << s->get_student_Code() << endl;}
+            break;
+        case '3':
+            newstudents3 = StudentSortCode(students);
+            cout << "Nome" << setw(15) << "Número de estudante" << endl;
+            for (Student *s : newstudents3){cout << s->get_Name() << "\t" << setw(9) << s->get_student_Code() << endl;}
+            break;
+        case '4':
+            newstudents4 = StudentReverseSortCode(students);
+            cout << "Nome" << setw(15) << "Número de estudante" << endl;
+            for (Student *s : newstudents4){cout << s->get_Name() << "\t" << setw(9) << s->get_student_Code() << endl;}
+            break;
     }
 }
